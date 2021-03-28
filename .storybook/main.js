d74287fd7c94429d740cbeb9001032319bbd378f
Hwang In Seo <sjsjsj1246@gmail.com>
2021-03-29 01:53:12 +0900

    build: 스토리북 초기 환경 설정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,9 @@
 module.exports = {
-  stories: [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-knobs',
+    '@storybook/addon-docs',
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
 };
